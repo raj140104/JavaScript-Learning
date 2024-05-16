a6597c44b16563f3d6f02548c5d3db9e14f55fd7
Ritesh Raj <riteshraj140104@gmail.com>
2024-05-16 11:34:45 +0000

    understanding of variable

--- basics/print.js
+++ basics/print.js
@@ -1,1 +1,11 @@
 console.log("Hello Ritesh Raj!")
+const name = "Ritesh"
+let age = 20;
+var state = "Bihar" // it does'nt follow Block Scope
+city = "Sh"
+
+console.log("After one year")
+age = 21
+state = "punjab"
+
+console.table([name, age, state, city])
