e494de2165902ff27b1188aa3cfa81119d2f7a49
Ritesh Raj <riteshraj140104@gmail.com>
2024-05-16 11:51:49 +0000

    Understanding of Datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+//String
+//BigInt
+//Number
+//Boolean
+//Symbol
+//undefined
+//null
+let name = "";
+let city;
+
+console.log(typeof("Ritesh"))
+console.log(typeof(""))
+console.log(typeof(123123213002))
+console.log(typeof(Symbol))
+console.log(typeof(true))
+console.log(typeof(city))
+console.log(typeof(name));
