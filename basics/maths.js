e2ede229c1b67bf5a070406830412d561f260b9f
Ritesh Raj <riteshraj140104@gmail.com>
2024-08-07 19:32:31 +0000

    Maths Formula

---
+++ basics/maths.js
@@ -0,0 +1,16 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toFixed(2))
+
+const num = 123.8788;
+console.log(num.toPrecision(4))
+
+//++++++++++++ Math ++++++++++++++++
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
